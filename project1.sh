248902d471b7730937745a6b1936f43567008a3d
Shweta <toshweta26@gmail.com>
2016-07-02 16:42:41 -0600

    project1.sh initial commit

---
+++ project1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Arguments
+
+# User Inputs
+
+
+# Get the last four changes#
+git log -4
+# Go to the git repository after initializing it
+# Or create the shell script at the same location as git repository
+
+# Look for the committed change that needs to be migrated
+
