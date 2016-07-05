cc1209a7cecf05b856274037682285e2132ad976
Shweta <toshweta26@gmail.com>
2016-07-05 16:05:40 -0600

    adding revertsinglechange function in the code

--- project1_July02.sh
+++ project1_July02.sh
@@ -8,14 +8,27 @@
 pushsinglechange()
 {
 read -rsp $'\n\nYou are about to move to a bitbucket branch.\n' -n1 key
-git checkout bitbucket #checking out a branch bitbucket which is in sync with the destination environment
+#1git checkout bitbucket #checking out a branch bitbucket which is in sync with the destination environment
 read -rsp $'\n\nYou are about to cherry-pick a change in a bitbucket branch.\n' -n1 key
 echo "Cherrypicking the selected change"
-git cherry-pick $singlechange
+#2git cherry-pick $singlechange
 read -rsp $'\n\nYou are about to push the cherry-picked change from the bitbucket branch.\n' -n1 key
-echo "Pushing the cherrypicked change"
-git push bitrepo
+echo "Pushing the cherrypicked change in the bitrepo repository"
+clear;
+#3git push bitrepo
+#4git checkout master
+clear;
+echo "The change has been pushed successfully."
 }
+
+# Function to revert single change
+revertsinglechange()
+{
+echo "Before reverting the change please specify the reason to do so."
+read reasontorevert
+
+}
+
 
 # Single change function
 singlechange()
@@ -33,9 +46,24 @@ git show --oneline $singlechange
 echo $'\n\n'
 echo "Are you sure you want to move the change $singlechange from $envorigin to $envdestination environment."
 read -rsp $' Press any key to continue or Ctrl+C to exit.' -n1 key
+
 pushsinglechange
 
-echo "You have successfully continued."
+echo "Enter 1 to revert the change."
+echo "Enter any other key to keep the change and continue."
+echo "Please enter your choice:"
+read revertchoice
+
+case $revertchoice in
+#case 1 in
+"1") 
+echo $'\n\n'
+echo "You chose to revert the change."
+revertsinglechange
+;;
+*) echo "You chose to keep the change."
+;;
+esac
 }
 
 # Multiple change function
@@ -54,6 +82,7 @@ echo "this is the multiple change block."
 
 
 # Get the last four changes#
+clear;
 echo "Which environment has your current code that needs to be moved?"
 read envorigin
 echo "In which environment do you want to move your code to?"
@@ -61,9 +90,9 @@ read envdestination
 clear;
 echo "Alright. So, the code needs to be moved from $envorigin to $envdestination environment."
 echo
-echo "Choose 1 to move a single change from $envorigin environment."
-echo "Choose 2 to move multiple changes from $envorigin environment."
-echo "Choose 3 to exit"
+echo "Enter 1 to move a single change from $envorigin environment."
+echo "Enter 2 to move multiple changes from $envorigin environment."
+echo "Enter 3 to exit"
 echo "Please enter your choice:"
 read choice
 
